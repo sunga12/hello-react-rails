04611986431f73c5446235ffeb5975a2fc6b4186
Sunga Thawethe <sungabt@gmail.com>
2023-11-09 10:50:20 +0200

    setup and fix linters

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Greeting from './components/Greeting';
-
+-m 
 function App() {
   return (
     <section>
